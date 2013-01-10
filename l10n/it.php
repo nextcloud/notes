0be852dbe31180635da0cc59af84082b3b374278
CaCO3 <caco3@ruinelli.ch>
2013-01-10 21:06:11 +0100

    Updated Italian and added Hungarian translation

--- l10n/it.php
+++ l10n/it.php
@@ -1,14 +1,14 @@
 <?php 
-//Copyright 2012 by Matteo Bettineschi
+//Copyright 2012-2013 by Matteo Bettineschi
 $TRANSLATIONS = array(
 "Notes" => "Note",
-"Add Note" => "Nuova Nota",
+"Add Note" => "Aggiungi Nota",
 "Edit Categories" => "Modifica Categorie",
 "Help" => "Aiuto",
 "Edit" => "Modifica",
 "Save" => "Salva",
 "Restore" => "Rispristina",
-"Restore content" => "Ripristina contenuto",
+"Restore content" => "Ripristina Contenuto",
 "Delete" => "Cancella",
 "Title" => "Titolo",
 "Content" => "Contenuto",
@@ -16,13 +16,14 @@ $TRANSLATIONS = array(
 "Categories" => "Categorie",
 "New category" => "Nuova Categoria",
 "Add" => "Aggiungi",
-"Back to Notes" => "Torna alla Nota",
+"Back to Notes" => "Torna alle Note",
 "Note" => "Note",
-"If you delete a category, all containing notes get moved to the following category" => "Se cancelli una categoria, tutte le note contenute saranno spostate nella seguente categoria",
+"If you delete a category, all containing notes get moved to the following category" => "Se cancelli una categoria tutte le note in essa contenute saranno spostate alla seguente categoria:",
 "No categories available yet" => "Nessuna categoria disponibile",
-"No note available in this category" => "Nessuna nota disponibile in questa categoria",
-"The title can not be empty" => "Il Titolo non può essere vuoto",
+"No note available in this category" => "Nessuna Nota presente in questa categoria",
+"The title can not be empty" => "Il titolo non può essere vuoto",
 "Invalid title" => "Titolo non valido",
-"The following characters are not allowed" => "I seguenti caratteri non sono ammessi",
+"The following characters are not allowed" => "I seguenti caratteri non sono permessi",
 "General" => "Generica"
 );
+ 
