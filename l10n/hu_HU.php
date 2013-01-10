eea4d05486f4b5c48f0c828be84ab829bf55d669
CaCO3 <caco3@ruinelli.ch>
2013-01-10 21:07:04 +0100

    Updated Italian and added Hungarian translation

---
+++ l10n/hu_HU.php
@@ -0,0 +1,29 @@
+<?php 
+//Copyright 2012-2013 by Tornóci László <tornoci.laszlo@med.semmelweis-univ.hu>
+$TRANSLATIONS = array(
+"Notes" => "Jegyzetek",
+"Add Note" => "Új jegyzet",
+"Edit Categories" => "A kategóriák módosítása",
+"Help" => "Súgó",
+"Edit" => "Szerkesztés",
+"Save" => "Mentés",
+"Restore" => "Visszaállítás",
+"Restore content" => "Visszaállítás egy másik változatra",
+"Delete" => "Törlés",
+"Title" => "Cím",
+"Content" => "Tartalom",
+"Category" => "Kategória",
+"Categories" => "Kategóriák",
+"New category" => "Új kategória",
+"Add" => "Hozzáadás",
+"Back to Notes" => "Vissza a Jegyzetekhez",
+"Note" => "Megjegyzés",
+"If you delete a category, all containing notes get moved to the following category" => "Ha megszüntet egy korábbi kategóriát, az ide tartozó jegyzetek automatikusan ebbe a kategóriába kerülnek át",
+"No categories available yet" => "Még egy kategóriát sem hozott létre",
+"No note available in this category" => "Ebben a kategóriában még nincs jegyzet",
+"The title can not be empty" => "A cím nem maradhat kitöltetlenül",
+"Invalid title" => "Érvénytelen cím",
+"The following characters are not allowed" => "A következő karaktereket nem szabad használni",
+"General" => "Általános"
+);
+ 
