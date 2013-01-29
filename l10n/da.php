20a9756bd0e2181c20999b993c8156200b683df8
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-01-30 00:22:14 +0100

    [tx-robot] updated from transifex

--- l10n/da.php
+++ l10n/da.php
@@ -1,11 +1,26 @@
 <?php $TRANSLATIONS = array(
+"Notes" => "Noter",
+"The title can not be empty" => "Titelfeltet kan ikke stå tomt",
+"Invalid title" => "Ugyldig titel",
+"The following characters are not allowed" => "Følgende tegn er ikke tilladte",
+"Back to Notes" => "Tilbage til Noter",
 "Help" => "Hjælp",
+"Categories" => "Kategorier",
 "Delete" => "Slet",
 "Save" => "Gem",
+"No categories available yet" => "Der er endnu ingen kategorier",
+"New category" => "Ny kategori",
 "Add" => "Tilføj",
 "Note" => "Note",
+"If you delete a category, all containing notes get moved to the following category" => "Hvis en kategori slettes, vil alle omfattede noter blive flyttet til følgende kategori",
 "General" => "Generel",
+"Add Note" => "Tilføj note",
+"Edit Categories" => "Rediger kategorier",
+"No note available in this category" => "Der er ingen note i denne kategori",
 "Edit" => "Rediger",
 "Category" => "Kategori",
-"Title" => "Titel"
+"Title" => "Titel",
+"Content" => "Indhold",
+"Restore content" => "Gendan indhold",
+"Restore" => "Gendan"
 );
