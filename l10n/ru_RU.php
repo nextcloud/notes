70467b47bef337c05d2afaa8094a3f20c112c88d
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-01-30 16:08:13 +0100

    [tx-robot] updated from transifex

--- l10n/ru_RU.php
+++ l10n/ru_RU.php
@@ -1,11 +1,23 @@
 <?php $TRANSLATIONS = array(
+"Notes" => "Заметки",
+"The title can not be empty" => "Название не может быть пустым",
+"Invalid title" => "Недействительное название",
+"The following characters are not allowed" => "Следующие символы недопустимы",
+"Back to Notes" => "Вернуться к заметкам",
 "Help" => "Помощь",
+"Categories" => "Категории",
 "Delete" => "Удалить",
 "Save" => "Сохранить",
+"No categories available yet" => "Нет доступных категорий",
+"New category" => "Новая категория",
 "Add" => "Добавить",
 "Note" => "Заметки",
+"If you delete a category, all containing notes get moved to the following category" => "Если Вы удалите категорию, все содержащиеся заметки будут перемещены в следующую категорию",
 "General" => "Общие",
+"Add Note" => "Добавить заметку",
+"Edit Categories" => "Редактировать категории",
 "Edit" => "Редактировать",
 "Category" => "Категория",
-"Title" => "Название"
+"Title" => "Название",
+"Content" => "Содержимое"
 );
