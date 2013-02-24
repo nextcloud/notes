40652b2fbb15de5cc4b705430c0647ca6b516d67
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-02-25 00:04:17 +0100

    [tx-robot] updated from transifex

--- l10n/he.php
+++ l10n/he.php
@@ -1,11 +1,4 @@
 <?php $TRANSLATIONS = array(
-"Help" => "עזרה",
-"Delete" => "מחיקה",
-"Save" => "שמירה",
-"Add" => "הוספה",
-"Note" => "הערה",
-"General" => "כללי",
-"Edit" => "עריכה",
-"Category" => "קטגוריה",
-"Title" => "כותרת"
+"Notes" => "הערות",
+"New Note" => "הערה חדשה"
 );
