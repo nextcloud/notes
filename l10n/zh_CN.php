fc80e9bf70cdcc07435e8c40a5d7a72e22b1dea2
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-03-02 00:03:19 +0100

    [tx-robot] updated from transifex

--- l10n/zh_CN.php
+++ l10n/zh_CN.php
@@ -1,11 +1,4 @@
 <?php $TRANSLATIONS = array(
-"Help" => "帮助",
-"Delete" => "删除",
-"Save" => "保存",
-"Add" => "增加",
-"Note" => "注释",
-"General" => "常规",
-"Edit" => "编辑",
-"Category" => "分类",
-"Title" => "标题"
+"Notes" => "笔记",
+"New Note" => "新笔记"
 );
