1d3c76c88271d4259e10a65d0ed65d06b8f192ee
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-04-26 01:21:58 +0200

    [tx-robot] updated from transifex

--- l10n/ro.php
+++ l10n/ro.php
@@ -1,11 +1,4 @@
 <?php $TRANSLATIONS = array(
-"Help" => "Ajutor",
-"Delete" => "Șterge",
-"Save" => "Salvează",
-"Add" => "Adaugă",
-"Note" => "Notă",
-"General" => "General",
-"Edit" => "Editează",
-"Category" => "Categorie",
-"Title" => "Titlu"
+"Notes" => "Notă",
+"New Note" => "Notiţă nouă"
 );
