ebb1587987538d87e7d40525852b80b6bbbbaec8
Bernhard Posselt <nukeawhale@gmail.com>
2013-08-18 01:38:49 +0200

    use dot instead of minus

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -61,31 +61,31 @@ $this->create('notes_delete', '/notes/{id}')->delete()->action(
 /**
  * API requests
  */
-$this->create('api_notes_get_all', '/api/v0-2/notes')->get()->action(
+$this->create('api_notes_get_all', '/api/v0.2/notes')->get()->action(
 	function($params){
 		App::main('NotesAPI', 'getAll', $params, new DIContainer());
 	}
 );
 
-$this->create('api_notes_get', '/api/v0-2/notes/{id}')->get()->action(
+$this->create('api_notes_get', '/api/v0.2/notes/{id}')->get()->action(
 	function($params){
 		App::main('NotesAPI', 'get', $params, new DIContainer());
 	}
 );
 
-$this->create('api_notes_create', '/api/v0-2/notes')->post()->action(
+$this->create('api_notes_create', '/api/v0.2/notes')->post()->action(
 	function($params){
 		App::main('NotesAPI', 'create', $params, new DIContainer());
 	}
 );
 
-$this->create('api_notes_update', '/api/v0-2/notes/{id}')->put()->action(
+$this->create('api_notes_update', '/api/v0.2/notes/{id}')->put()->action(
 	function($params){
 		App::main('NotesAPI', 'update', $params, new DIContainer());
 	}
 );
 
-$this->create('api_notes_delete', '/api/v0-2/notes/{id}')->delete()->action(
+$this->create('api_notes_delete', '/api/v0.2/notes/{id}')->delete()->action(
 	function($params){
 		App::main('NotesAPI', 'delete', $params, new DIContainer());
 	}
