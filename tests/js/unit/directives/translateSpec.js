6b316003d503678c01674e1b84a6415c0cb7df42
Bernhard Posselt <nukeawhale@gmail.com>
2013-08-18 11:51:09 +0200

    first test for directive

---
+++ tests/js/unit/directives/translateSpec.js
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2013, Bernhard Posselt <nukeawhale@gmail.com>
+ * This file is licensed under the Affero General Public License version 3 or later.
+ * See the COPYING file.
+ */
+
+
+describe('notesTranslate', function() {
+
+	var host,
+		rootScope,
+		compile;
+
+	beforeEach(module('Notes'));
+
+	beforeEach(inject(function($rootScope, $compile) {
+		rootScope = $rootScope;
+		compile = $compile;
+		host = $('<div id="host"></div>');
+		$('body').append(host);
+
+	}));
+
+
+	it ('should be set the translated string on scope', function () {
+		var element = angular.element(
+			'<notes-translate key="is">translated</notes-translate>'
+		);
+		compile(element)(rootScope);
+		rootScope.$digest();
+		host.append(element);
+
+		expect(rootScope.translations['is']).toBe('translated');
+	});
+
+
+	afterEach(function () {
+		host.remove();
+	});
+
+
+});
