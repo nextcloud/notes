673ed9dd067efccacd0037dc76a345d949da9a33
Bernhard Posselt <dev@bernhard-posselt.com>
2014-02-14 14:13:24 +0100

    version bump

--- js/app/directives/markdown.js
+++ js/app/directives/markdown.js
@@ -5,18 +5,18 @@
  */
 
 app.directive('markdown', function () {
-		return {
-			restrict: 'AE',
-			link: function (scope, element, attrs) {
-				if (attrs.markdown) {
-					scope.$watch(attrs.markdown, function (newVal) {
-						var html = markdown.toHTML(newVal);
-						element.html(html);
-					});
-				} else {
-					var html = markdown.toHTML(element.text());
+	return {
+		restrict: 'AE',
+		link: function (scope, element, attrs) {
+			if (attrs.markdown) {
+				scope.$watch(attrs.markdown, function (newVal) {
+					var html = markdown.toHTML(newVal);
 					element.html(html);
-				}
+				});
+			} else {
+				var html = markdown.toHTML(element.text());
+				element.html(html);
 			}
-		};
-	});
+		}
+	};
+});
