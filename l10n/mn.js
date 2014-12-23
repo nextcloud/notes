09275738b2bcc05fdac0af5715acdef43dcd94a6
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-12-23 01:21:16 -0500

    [tx-robot] updated from transifex

---
+++ l10n/mn.js
@@ -0,0 +1,8 @@
+OC.L10N.register(
+    "notes",
+    {
+    "Notes" : "Тэмдэглэгээ",
+    "New note" : "Шинэ тэмдэглэгээ",
+    "Delete note" : "Тэмдэглэгээ устгах"
+},
+"nplurals=2; plural=(n != 1);");
---
+++ l10n/mn.php
@@ -0,0 +1,7 @@
+<?php
+$TRANSLATIONS = array(
+"Notes" => "Тэмдэглэгээ",
+"New note" => "Шинэ тэмдэглэгээ",
+"Delete note" => "Тэмдэглэгээ устгах"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
