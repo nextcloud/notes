621303119a41121c661b5e8806b253db2e6b1ae0
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-03-28 01:21:15 -0400

    [tx-robot] updated from transifex

--- l10n/ca.js
+++ l10n/ca.js
@@ -2,6 +2,7 @@ OC.L10N.register(
     "notes",
     {
     "Notes" : "Anotacions",
+    "Note is currently saving. Leaving " : "La nota s'està guardant. Sortir",
     "New note" : "Nova anotació",
     "Delete note" : "Esborra l'anotació"
 },
--- l10n/ca.php
+++ l10n/ca.php
@@ -1,6 +1,7 @@
 <?php
 $TRANSLATIONS = array(
 "Notes" => "Anotacions",
+"Note is currently saving. Leaving " => "La nota s'està guardant. Sortir",
 "New note" => "Nova anotació",
 "Delete note" => "Esborra l'anotació"
 );
