f56f9967b966cfafaaed813172c8c5719959943c
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-06-06 01:21:24 -0400

    [tx-robot] updated from transifex

---
+++ l10n/th_TH.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "notes",
+    {
+    "Notes" : "บันทึกย่อ",
+    "New note" : "สร้างบันทึกใหม่",
+    "Note is currently saving. Leaving " : "ในขณะนี้กำลังบันทึก บันทึกย่อและออก",
+    "Delete note" : "ลบบันทึกย่อ"
+},
+"nplurals=1; plural=0;");
--- l10n/th_TH.php
+++ l10n/th_TH.php
@@ -1,5 +1,8 @@
 <?php
 $TRANSLATIONS = array(
-"Notes" => "บันทึกย่อ"
+"Notes" => "บันทึกย่อ",
+"New note" => "สร้างบันทึกใหม่",
+"Note is currently saving. Leaving " => "ในขณะนี้กำลังบันทึก บันทึกย่อและออก",
+"Delete note" => "ลบบันทึกย่อ"
 );
 $PLURAL_FORMS = "nplurals=1; plural=0;";
