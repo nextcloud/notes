546501e928c8944b21a73e7ceab6fff139c93286
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-09-04 01:21:35 -0400

    [tx-robot] updated from transifex

--- l10n/fa.js
+++ l10n/fa.js
@@ -3,6 +3,7 @@ OC.L10N.register(
     {
     "Notes" : "یادداشت ها",
     "New note" : "یادداشت جدید",
+    "Note is currently saving. Leaving " : "یادداشت در حال ذخیره شدن می باشد . در حال ترک کردن",
     "Delete note" : "حذف یادداشت"
 },
 "nplurals=1; plural=0;");
--- l10n/fa.php
+++ l10n/fa.php
@@ -2,6 +2,7 @@
 $TRANSLATIONS = array(
 "Notes" => "یادداشت ها",
 "New note" => "یادداشت جدید",
+"Note is currently saving. Leaving " => "یادداشت در حال ذخیره شدن می باشد . در حال ترک کردن",
 "Delete note" => "حذف یادداشت"
 );
 $PLURAL_FORMS = "nplurals=1; plural=0;";
