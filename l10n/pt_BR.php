a64fbe9c7d0e4e62a0209d25d03e02f91fcc9401
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-09-19 01:21:28 -0400

    [tx-robot] updated from transifex

--- l10n/pt_BR.php
+++ l10n/pt_BR.php
@@ -3,6 +3,7 @@ $TRANSLATIONS = array(
 "Notes" => "Notas",
 "New note" => "Nova nota",
 "Note is currently saving. Leaving " => "Nota está sendo salva. Partindo",
+"_%n word_::_%n words_" => array("%n palavra","%n palavras"),
 "Delete note" => "Eliminar nota"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n > 1);";
