1c18daa83e9bd878dfad4ea8d28ea4f5bd426389
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-09-24 01:21:28 -0400

    [tx-robot] updated from transifex

--- l10n/id.js
+++ l10n/id.js
@@ -4,6 +4,7 @@ OC.L10N.register(
     "Notes" : "Catatan",
     "New note" : "Catatan baru",
     "Note is currently saving. Leaving " : "Catatan sudah disimpan. Meninggalkan",
+    "_%n word_::_%n words_" : ["%n kata"],
     "Delete note" : "Hapus catatan"
 },
 "nplurals=1; plural=0;");
--- l10n/id.php
+++ l10n/id.php
@@ -3,6 +3,7 @@ $TRANSLATIONS = array(
 "Notes" => "Catatan",
 "New note" => "Catatan baru",
 "Note is currently saving. Leaving " => "Catatan sudah disimpan. Meninggalkan",
+"_%n word_::_%n words_" => array("%n kata"),
 "Delete note" => "Hapus catatan"
 );
 $PLURAL_FORMS = "nplurals=1; plural=0;";
