50488583da830dd74a06ddafdc3692276eef182c
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-10-04 01:21:26 -0400

    [tx-robot] updated from transifex

--- l10n/ko.php
+++ l10n/ko.php
@@ -3,6 +3,7 @@ $TRANSLATIONS = array(
 "Notes" => "메모",
 "New note" => "새 메모",
 "Note is currently saving. Leaving " => "메모를 저장하고 있습니다. 떠나는 중",
+"_%n word_::_%n words_" => array("단어 %n개"),
 "Delete note" => "메모 삭제"
 );
 $PLURAL_FORMS = "nplurals=1; plural=0;";
