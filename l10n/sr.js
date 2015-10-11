3616bbc4423eac22399e7572b7eb52fb9d807050
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-10-11 01:21:28 -0400

    [tx-robot] updated from transifex

--- l10n/sr.js
+++ l10n/sr.js
@@ -4,6 +4,7 @@ OC.L10N.register(
     "Notes" : "Белешке",
     "New note" : "Нова белешка",
     "Note is currently saving. Leaving " : "Белешка се тренутно снима. Излазим",
+    "_%n word_::_%n words_" : ["%n реч","%n речи","%n речи"],
     "Delete note" : "Обриши белешку"
 },
 "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);");
--- l10n/sr.php
+++ l10n/sr.php
@@ -3,6 +3,7 @@ $TRANSLATIONS = array(
 "Notes" => "Белешке",
 "New note" => "Нова белешка",
 "Note is currently saving. Leaving " => "Белешка се тренутно снима. Излазим",
+"_%n word_::_%n words_" => array("%n реч","%n речи","%n речи"),
 "Delete note" => "Обриши белешку"
 );
 $PLURAL_FORMS = "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);";
