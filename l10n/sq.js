fecc0e66ae025f33df24501d8a68a694f28a1f56
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-10-16 01:21:27 -0400

    [tx-robot] updated from transifex

--- l10n/sq.js
+++ l10n/sq.js
@@ -2,7 +2,9 @@ OC.L10N.register(
     "notes",
     {
     "Notes" : "Shënime",
-    "New note" : "Shënime t'reja",
-    "Delete note" : "Fshi shënimet"
+    "New note" : "Shënim i ri",
+    "Note is currently saving. Leaving " : "Shënimi po ruhet. Po dilet",
+    "_%n word_::_%n words_" : ["%n fjalë","%n fjalë"],
+    "Delete note" : "Fshije shënimin"
 },
 "nplurals=2; plural=(n != 1);");
--- l10n/sq.php
+++ l10n/sq.php
@@ -1,7 +1,9 @@
 <?php
 $TRANSLATIONS = array(
 "Notes" => "Shënime",
-"New note" => "Shënime t'reja",
-"Delete note" => "Fshi shënimet"
+"New note" => "Shënim i ri",
+"Note is currently saving. Leaving " => "Shënimi po ruhet. Po dilet",
+"_%n word_::_%n words_" => array("%n fjalë","%n fjalë"),
+"Delete note" => "Fshije shënimin"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
