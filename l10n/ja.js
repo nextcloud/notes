656c17043333ae678ecdf5610201652a12ef975a
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-10-21 01:21:27 -0400

    [tx-robot] updated from transifex

--- l10n/ja.js
+++ l10n/ja.js
@@ -4,6 +4,7 @@ OC.L10N.register(
     "Notes" : "ノート",
     "New note" : "新しいノート",
     "Note is currently saving. Leaving " : "ノートを保存しています。しばらくお待ちください。",
+    "_%n word_::_%n words_" : ["%n 単語"],
     "Delete note" : "ノートを削除"
 },
 "nplurals=1; plural=0;");
--- l10n/ja.php
+++ l10n/ja.php
@@ -3,6 +3,7 @@ $TRANSLATIONS = array(
 "Notes" => "ノート",
 "New note" => "新しいノート",
 "Note is currently saving. Leaving " => "ノートを保存しています。しばらくお待ちください。",
+"_%n word_::_%n words_" => array("%n 単語"),
 "Delete note" => "ノートを削除"
 );
 $PLURAL_FORMS = "nplurals=1; plural=0;";
