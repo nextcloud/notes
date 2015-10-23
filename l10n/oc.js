e084c41e65b0e031e30f7a228b3417bcb3bf12ec
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-10-23 01:21:28 -0400

    [tx-robot] updated from transifex

--- l10n/oc.js
+++ l10n/oc.js
@@ -4,6 +4,7 @@ OC.L10N.register(
     "Notes" : "Nòtas",
     "New note" : "Nòta novèla",
     "Note is currently saving. Leaving " : "Nòta en cors de salvament. Sortida en cors...",
+    "_%n word_::_%n words_" : ["%n mot","%n mots"],
     "Delete note" : "Suprimir la nòta"
 },
 "nplurals=2; plural=(n > 1);");
--- l10n/oc.php
+++ l10n/oc.php
@@ -3,6 +3,7 @@ $TRANSLATIONS = array(
 "Notes" => "Nòtas",
 "New note" => "Nòta novèla",
 "Note is currently saving. Leaving " => "Nòta en cors de salvament. Sortida en cors...",
+"_%n word_::_%n words_" => array("%n mot","%n mots"),
 "Delete note" => "Suprimir la nòta"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n > 1);";
