a82fcbfe2450a2f93eba0543618047d6504774ad
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-11-03 01:21:32 -0500

    [tx-robot] updated from transifex

--- l10n/zh_TW.js
+++ l10n/zh_TW.js
@@ -3,6 +3,8 @@ OC.L10N.register(
     {
     "Notes" : "筆記",
     "New note" : "新筆記",
+    "Note is currently saving. Leaving " : "正在儲存筆記 離開",
+    "_%n word_::_%n words_" : ["%n 字"],
     "Delete note" : "刪除筆記"
 },
 "nplurals=1; plural=0;");
--- l10n/zh_TW.php
+++ l10n/zh_TW.php
@@ -2,6 +2,8 @@
 $TRANSLATIONS = array(
 "Notes" => "筆記",
 "New note" => "新筆記",
+"Note is currently saving. Leaving " => "正在儲存筆記 離開",
+"_%n word_::_%n words_" => array("%n 字"),
 "Delete note" => "刪除筆記"
 );
 $PLURAL_FORMS = "nplurals=1; plural=0;";
