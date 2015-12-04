28579ca15f5de11eb9f461dee83eb5d03cacfaa9
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-12-04 01:21:24 -0500

    [tx-robot] updated from transifex

--- l10n/lt_LT.php
+++ l10n/lt_LT.php
@@ -2,6 +2,7 @@
 $TRANSLATIONS = array(
 "Notes" => "Pastabos",
 "New note" => "Nauja pastaba",
+"Note is currently saving. Leaving " => "Užrašas išsaugomas. Uždaroma",
 "Delete note" => "Ištrinti pastabą"
 );
 $PLURAL_FORMS = "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && (n%100<10 || n%100>=20) ? 1 : 2);";
