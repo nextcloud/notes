32208be55d2ac5fb4bc493c1c211ed84cb24c5ce
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-03-19 01:21:27 -0400

    [tx-robot] updated from transifex

--- l10n/sv.js
+++ l10n/sv.js
@@ -3,6 +3,8 @@ OC.L10N.register(
     {
     "Notes" : "Anteckningar",
     "New note" : "Ny anteckning",
+    "Note is currently saving. Leaving " : "Anteckning sparas just nu.",
+    "_%n word_::_%n words_" : ["%n ord","%n ord"],
     "Delete note" : "Radera anteckning"
 },
 "nplurals=2; plural=(n != 1);");
--- l10n/sv.php
+++ l10n/sv.php
@@ -2,6 +2,8 @@
 $TRANSLATIONS = array(
 "Notes" => "Anteckningar",
 "New note" => "Ny anteckning",
+"Note is currently saving. Leaving " => "Anteckning sparas just nu.",
+"_%n word_::_%n words_" => array("%n ord","%n ord"),
 "Delete note" => "Radera anteckning"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
