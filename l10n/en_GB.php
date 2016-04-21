dfa6327cf956c081b393122f43c8bb07eb09709a
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-04-21 01:21:28 -0400

    [tx-robot] updated from transifex

--- l10n/en_GB.php
+++ l10n/en_GB.php
@@ -2,6 +2,7 @@
 $TRANSLATIONS = array(
 "Notes" => "Notes",
 "New note" => "New note",
+"Note is currently saving. Leaving the page will delete all changes!" => "Note is currently saving. Leaving the page will delete all changes!",
 "_%n word_::_%n words_" => array("%n word","%n words"),
 "Delete note" => "Delete note"
 );
