3f68fb100ebcdd2817e7e009391ce6142a412ae6
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-05-05 01:21:29 -0400

    [tx-robot] updated from transifex

--- l10n/ja.js
+++ l10n/ja.js
@@ -3,6 +3,7 @@ OC.L10N.register(
     {
     "Notes" : "ノート",
     "New note" : "新しいノート",
+    "Note is currently saving. Leaving the page will delete all changes!" : "ノートを保存しています。このページから移動する場合は全ての変更が削除されます！",
     "_%n word_::_%n words_" : ["%n 単語"],
     "Delete note" : "ノートを削除"
 },
--- l10n/ja.php
+++ l10n/ja.php
@@ -2,6 +2,7 @@
 $TRANSLATIONS = array(
 "Notes" => "ノート",
 "New note" => "新しいノート",
+"Note is currently saving. Leaving the page will delete all changes!" => "ノートを保存しています。このページから移動する場合は全ての変更が削除されます！",
 "_%n word_::_%n words_" => array("%n 単語"),
 "Delete note" => "ノートを削除"
 );
