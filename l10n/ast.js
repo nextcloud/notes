90e097bac6ed42ac9fa9a9084f76e53c6c886c49
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-06-29 01:21:28 -0400

    [tx-robot] updated from transifex

--- l10n/ast.js
+++ l10n/ast.js
@@ -3,6 +3,8 @@ OC.L10N.register(
     {
     "Notes" : "Notes",
     "New note" : "Nota nueva",
+    "Note is currently saving. Leaving the page will delete all changes!" : "La nota ta guardandose. Si de la páxina, ¡van esaniciase tolos cambeos!",
+    "_%n word_::_%n words_" : ["%n pallabra","%n pallabres"],
     "Delete note" : "Desaniciar nota"
 },
 "nplurals=2; plural=(n != 1);");
--- l10n/ast.php
+++ l10n/ast.php
@@ -2,6 +2,8 @@
 $TRANSLATIONS = array(
 "Notes" => "Notes",
 "New note" => "Nota nueva",
+"Note is currently saving. Leaving the page will delete all changes!" => "La nota ta guardandose. Si de la páxina, ¡van esaniciase tolos cambeos!",
+"_%n word_::_%n words_" => array("%n pallabra","%n pallabres"),
 "Delete note" => "Desaniciar nota"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
