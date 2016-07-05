82c1498afb3851b2bfdfc46499534c889edc1648
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-07-05 01:21:32 -0400

    [tx-robot] updated from transifex

--- l10n/nb_NO.js
+++ l10n/nb_NO.js
@@ -3,6 +3,7 @@ OC.L10N.register(
     {
     "Notes" : "Notater",
     "New note" : "Nytt notat",
+    "Note is currently saving. Leaving the page will delete all changes!" : "Notatet lagres. Alle endringer blir forkastet hvis du forlater siden!",
     "_%n word_::_%n words_" : ["%n ord","%n ord"],
     "Delete note" : "Slett notat"
 },
