8167ed4f89d50f6eda36566178e023c949ca323d
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-07-07 01:22:53 -0400

    [tx-robot] updated from transifex

--- l10n/el.js
+++ l10n/el.js
@@ -3,6 +3,7 @@ OC.L10N.register(
     {
     "Notes" : "Σημειώσεις",
     "New note" : "Νέα σημείωση",
+    "Note is currently saving. Leaving the page will delete all changes!" : "Σημείωση προς το παρόν είναι η αποταμίευση. Αφήστε τη σελίδα θα διαγράψει όλες τις αλλαγές!",
     "_%n word_::_%n words_" : ["%n λέξη","%n λέξεις"],
     "Delete note" : "Διαγραφή σημείωσης"
 },
