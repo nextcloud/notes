7867d59337dc82dff765df3c9c87cd758d896dc6
korelstar <korelstar@users.noreply.github.com>
2017-01-22 10:56:27 +0100

    Fix navigation sidebar toggle

--- templates/note.php
+++ templates/note.php
@@ -1,4 +1,2 @@
-<div id="app-navigation-toggle" class="icon-menu" style="display:none;"></div>
 	<pre editor notes-timeout-change="save()" notes-autofocus class="editor" ng-class="{saving: isSaving()}">{{note.content}}</pre>
 	<div class="note-meta">{{note.content | wordCount}}</div>
-</div>
