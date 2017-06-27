966629db22ba4fafe67bad862b5f09c1170bbcd2
Andreas Jacobsen <andreasjacobsen93@gmail.com>
2017-06-27 17:02:24 +0200

    Fixed weird fullscreen-button size (#104)
    * Added class to button
    
    * Update notes.css

--- templates/note.php
+++ templates/note.php
@@ -2,6 +2,6 @@
 	<div class="note-meta">
 		<span class="note-word-count" ng-if="note.content.length > 0">{{note.content | wordCount}}</span>
 		<span class="note-meta-right">
-			<button class="icon-fullscreen has-tooltip" notes-tooltip ng-click="toggleDistractionFree()"></button>
+			<button class="icon-fullscreen has-tooltip btn-fullscreen" notes-tooltip ng-click="toggleDistractionFree()"></button>
 		</span>
 	</div>
