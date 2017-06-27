8a9e92debec5fc3d4413b6e058fe8b481f694925
Andreas Jacobsen <andreasjacobsen93@gmail.com>
2017-06-27 15:40:23 +0200

    Don't show word count on empty note

--- templates/note.php
+++ templates/note.php
@@ -1,6 +1,6 @@
 	<textarea editor notes-timeout-change="save()" name="editor"></textarea>
 	<div class="note-meta">
-		{{note.content | wordCount}}
+		<span class="note-word-count" ng-if="note.content.length > 0">{{note.content | wordCount}}</span>
 		<span class="note-meta-right">
 			<button class="icon-fullscreen has-tooltip" notes-tooltip ng-click="toggleDistractionFree()"></button>
 		</span>
