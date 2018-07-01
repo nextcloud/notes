7e1cb79a3acc4857d48c41c868d7860d24b5f4af
korelstar <korelstar@users.noreply.github.com>
2018-07-01 21:10:30 +0200

    show error message if notes folder has errors

---
+++ service/notesfolderexception.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Nextcloud - Notes
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ */
+
+namespace OCA\Notes\Service;
+
+use Exception;
+
+/**
+ * Class NotesFolderException
+ *
+ * @package OCA\Notes\Service
+ */
+class NotesFolderException extends Exception {}
