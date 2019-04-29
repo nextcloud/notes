119ab1f32ddf855f64f708a5a5812ad09d74cfb2
korelstar <korelstar@users.noreply.github.com>
2019-04-29 08:02:30 +0200

    welcome screen with some introduction text

--- src/Welcome.vue
+++ src/Welcome.vue
@@ -1,6 +1,13 @@
 <template>
 	<AppContent>
-		<p>TODO: Welcome-Screen</p>
+		<div class="welcome-content">
+			<h2>{{ tn('Notes') }}</h2>
+			<p class="feature icon-add">{{ tn('Start writing a note by clicking on “{newnote}” in the app navigation.', { newnote: tn('New note') }) }}</p>
+			<p class="feature icon-fullscreen">{{ tn('Write down your thoughts without any distractions.') }}</p>
+			<p class="feature icon-toggle-filelist">{{ tn('Use Markdown markups to style your text.') }}</p>
+			<p class="feature icon-tag">{{ tn('Organize your notes in categories.') }}</p>
+			<p class="feature icon-phone">{{ tn('Install the Android app or iOS app in order to access your notes from everywhere.') }}</p>
+		</div>
 	</AppContent>
 </template>
 <script>
@@ -18,3 +25,19 @@ export default {
 }
 
 </script>
+<style>
+.welcome-content {
+	padding: 2em 3em;
+}
+
+.welcome-content h2 {
+	margin-bottom: 1em;
+}
+
+.feature {
+	background-position: left top;
+	min-height: 32px;
+	padding-left: 32px;
+	margin-top: 1em;
+}
+</style>
