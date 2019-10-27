4d2b3a406eb1401161441e181bcc11d637c087bb
korelstar <korelstar@users.noreply.github.com>
2019-10-27 09:51:15 +0100

    update eslint-config-nextcloud

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,14 +6,6 @@ module.exports = {
 		$: 'readonly',
 	},
 	rules: {
-		// always add a trailing comma, for diff readability (was "never" in "standard")
-		'comma-dangle': ['warn', 'always-multiline'],
-
-		// disallow use of "var" (not in "standard")
-		'no-var': 'error',
-		// Suggest using const
-		'prefer-const': 'warn',
-
 		// no ending html tag on a new line (was warn in "vue/strongly-recommended")
 		'vue/html-closing-bracket-newline': ['error', { multiline: 'always' }]
 	},
