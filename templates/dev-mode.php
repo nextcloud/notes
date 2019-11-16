d218ca9d8b9a83401bb9064236193aeaa6f110d6
korelstar <korelstar@users.noreply.github.com>
2019-11-16 22:53:12 +0100

    warning if JavaScript was not built

---
+++ templates/dev-mode.php
@@ -0,0 +1,4 @@
+<div id="app-content" style="margin: 2em 3em;">
+	<h2><?php echo $l->t('Development Mode'); ?></h2>
+	<p><?php echo $l->t('It looks that the notes app was installed from a development source. Please install it from the official <a href="%1$s">Nextcloud App Store</a> instead. If you want to build the Notes app by yourself, please follow the <a href="%2$s">developer instructions</a>.', [ 'https://apps.nextcloud.com/apps/notes', 'https://github.com/nextcloud/notes#warning-developer-info' ]); ?></p>
+</div>
