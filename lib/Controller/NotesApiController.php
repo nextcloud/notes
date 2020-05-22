43105a21dd819c30ec971cffa26084d005aabbab
korelstar <korelstar@users.noreply.github.com>
2020-05-22 11:28:39 +0200

    fix wrong result type

--- lib/Controller/NotesApiController.php
+++ lib/Controller/NotesApiController.php
@@ -53,9 +53,9 @@ class NotesApiController extends ApiController {
 			$now = new \DateTime(); // this must be before loading notes if there are concurrent changes possible
 			$notes = $this->service->getAll($this->getUID())['notes'];
 			if ($category !== null) {
-				$notes = array_filter($notes, function ($note) use ($category) {
+				$notes = array_values(array_filter($notes, function ($note) use ($category) {
 					return $note->getCategory() === $category;
-				});
+				}));
 			}
 			$metas = $this->metaService->updateAll($this->getUID(), $notes);
 			$notesData = array_map(function ($note) use ($metas, $pruneBefore, $exclude) {
