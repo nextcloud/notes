ca89679fbd57756f6a2b4d80643235f036248192
korelstar <korelstar@users.noreply.github.com>
2020-06-05 23:09:39 +0200

    remove jQuery usage

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,6 @@ module.exports = {
 	extends: [
 		'@nextcloud',
 	],
-	globals: {
-		$: 'readonly',
-	},
 	rules: {
 		// no ending html tag on a new line (was warn in "vue/strongly-recommended")
 		'vue/html-closing-bracket-newline': ['error', { multiline: 'always' }],
