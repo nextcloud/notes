f88257f0d577e3b9d602b4592701be46ee6ee4b2
korelstar <korelstar@users.noreply.github.com>
2020-06-12 22:24:27 +0200

    add app version to capabilities

--- lib/Capabilities.php
+++ lib/Capabilities.php
@@ -3,13 +3,22 @@
 namespace OCA\Notes;
 
 use OCP\Capabilities\ICapability;
+use OCP\App\IAppManager;
 
 class Capabilities implements ICapability {
+
+	/** @var IAppManager */
+	private $appManager;
+
+	public function __construct(IAppManager $appManager) {
+		$this->appManager = $appManager;
+	}
 
 	public function getCapabilities() {
 		return [
 			'notes' => [
 				'api_version' => Application::$API_VERSIONS,
+				'version' => $this->appManager->getAppVersion('notes'),
 			],
 		];
 	}
