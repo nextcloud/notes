93d1068cf28bbf242e7b17ef1bb65fa17b4b892b
korelstar <korelstar@users.noreply.github.com>
2021-04-02 13:10:43 +0200

    API: new minor API version 1.2

--- tests/api/APIv1Test.php
+++ tests/api/APIv1Test.php
@@ -6,6 +6,6 @@ namespace OCA\Notes\Tests\API;
 
 class APIv1Test extends CommonAPITest {
 	public function __construct() {
-		parent::__construct('1.1', false);
+		parent::__construct('1.2', false);
 	}
 }
