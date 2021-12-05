6b3453631bcc4a53515278eaaadbec2bae9b1e57
korelstar <korelstar@users.noreply.github.com>
2021-12-05 20:29:49 +0100

    update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,5 +5,7 @@ module.exports = {
 	rules: {
 		// no ending html tag on a new line (was warn in "vue/strongly-recommended")
 		'vue/html-closing-bracket-newline': ['error', { multiline: 'always' }],
+		// do not require JSDoc comments
+		'jsdoc/require-jsdoc': 'off',
 	},
 }
