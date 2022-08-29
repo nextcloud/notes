7d775bf58396984e8ff7b951c36ae5254e472989
Robin Appelman <robin@icewind.nl>
2022-08-29 16:14:48 +0200

    implement client dashboard api

--- lib/AppInfo/DashboardWidget.php
+++ lib/AppInfo/DashboardWidget.php
@@ -4,20 +4,28 @@ declare(strict_types=1);
 
 namespace OCA\Notes\AppInfo;
 
+use OCA\Notes\Service\Note;
+use OCA\Notes\Service\NotesService;
+use OCP\Dashboard\IAPIWidget;
+use OCP\Dashboard\IButtonWidget;
 use OCP\Dashboard\IWidget;
+use OCP\Dashboard\Model\WidgetItem;
 use OCP\IL10N;
 use OCP\IURLGenerator;
 
-class DashboardWidget implements IWidget {
+class DashboardWidget implements IWidget, IButtonWidget, IAPIWidget {
 	private IURLGenerator $url;
 	private IL10N $l10n;
+	private NotesService $notesService;
 
 	public function __construct(
 		IURLGenerator $url,
-		IL10N $l10n
+		IL10N $l10n,
+		NotesService $notesService
 	) {
 		$this->url = $url;
 		$this->l10n = $l10n;
+		$this->notesService = $notesService;
 	}
 
 	/**
@@ -60,5 +68,32 @@ class DashboardWidget implements IWidget {
 	 */
 	public function load(): void {
 		\OCP\Util::addScript('notes', 'notes-dashboard');
+	}
+
+	public function getButtonUrl(): string {
+		return $this->url->linkToRouteAbsolute('notes.page.create');
+	}
+
+	public function getButtonIconUrl(): ?string {
+		return null;
+	}
+
+	public function getButtonText(): string {
+		return $this->l10n->t('New notes');
+	}
+
+	public function getItems(string $userId, ?string $since = null, int $limit = 7): array {
+		$notes = $this->notesService->getTopNotes($userId);
+		$notes = array_slice($notes, 0, $limit);
+		return array_map(function (Note $note) {
+			$excerpt = '';
+			try {
+				$excerpt = $note->getExcerpt();
+			} catch (\Throwable $e) {
+			}
+			$link = $this->url->linkToRouteAbsolute('notes.page.index', ['id' => $note->getId()]);
+			$icon = $note->getFavorite() ? $this->url->getAbsoluteURL($this->url->imagePath('core', 'actions/starred.svg')) : '';
+			return new WidgetItem($note->getTitle(), $excerpt, $link, $icon, (string)$note->getModified());
+		}, $notes);
 	}
 }
