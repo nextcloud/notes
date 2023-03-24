21343073d4c4dfe35c07662c504f744733d00663
Jonathan Pagel <jonny_tischbein@systemli.org>
2023-03-24 22:21:55 +0100

    Category: Add new category button to create new folders in current notes path for categories.
    Signed-off-by: Jonathan Pagel <jonny_tischbein@systemli.org>

--- src/components/CategoriesList.vue
+++ src/components/CategoriesList.vue
@@ -1,5 +1,38 @@
 <template>
 	<Fragment>
+
+	<NcAppNavigationItem
+		:title="title"
+		class="app-navigation-noclose separator-below"
+		:class="{ 'category-header': selectedCategory !== null }"
+		:open.sync="open"
+		:menu-open.sync="menuOpen"
+		:allow-collapse="true"
+		@click.prevent.stop="onToggleCategories"
+	>
+		<template #menu-icon>
+			<AddIcon :size="20" @click="onToggleNewCategory" />
+		</template>
+		<template #actions>
+			<NcActionText>
+				<template #icon>
+					<ErrorIcon v-if="createCategoryError" :size="20" />
+					<AddIcon v-else-if="!createCategoryError" :size="20" />
+				</template>
+				{{ createCategoryError ? createCategoryError : t('notes', 'Create a new category') }}
+			</NcActionText>
+			<NcActionInput
+				icon=""
+				:value="t('notes', 'Category name')"
+				@submit.prevent.stop="createNewCategory"
+			>
+				<template #icon>
+					<FolderIcon :size="20" />
+				</template>
+			</NcActionInput>
+		</template>
+
+		<FolderIcon slot="icon" :size="20" />
 		<NcAppNavigationItem
 			:title="t('notes', 'All notes')"
 			:class="{ active: selectedCategory === null }"
@@ -31,6 +64,8 @@
 
 <script>
 import {
+	NcActionInput,
+	NcActionText,
 	NcAppNavigationItem,
 	NcAppNavigationCaption,
 	NcAppNavigationCounter,
@@ -40,8 +75,10 @@ import { Fragment } from 'vue-fragment'
 import FolderIcon from 'vue-material-design-icons/Folder.vue'
 import FolderOutlineIcon from 'vue-material-design-icons/FolderOutline.vue'
 import HistoryIcon from 'vue-material-design-icons/History.vue'
+import AddIcon from 'vue-material-design-icons/Plus.vue'
+import ErrorIcon from 'vue-material-design-icons/AlertCircle.vue'
 
-import { getCategories } from '../NotesService.js'
+import { createCategory, findCategory, getCategories } from '../NotesService.js'
 import { categoryLabel } from '../Util.js'
 import store from '../store.js'
 
@@ -56,6 +93,18 @@ export default {
 		FolderIcon,
 		FolderOutlineIcon,
 		HistoryIcon,
+		AddIcon,
+		ErrorIcon,
+		NcActionInput,
+		NcActionText,
+	},
+
+	data() {
+		return {
+			open: false,
+			menuOpen: false,
+			createCategoryError: null,
+		}
 	},
 
 	computed: {
@@ -77,8 +126,36 @@ export default {
 			return categoryLabel(category)
 		},
 
+		onToggleCategories() {
+			this.open = !this.open
+		},
+
+		onToggleNewCategory() {
+			this.menuOpen = !this.menuOpen
+		},
+
 		onSelectCategory(category) {
 			store.commit('setSelectedCategory', category)
+		},
+
+		createNewCategory(event) {
+			const input = event.target.querySelector('input[type=text]')
+			const categoryName = input.value.trim()
+
+			// Check if already exists
+			findCategory(categoryName).then(data => {
+				if (data !== false) {
+					this.createCategoryError = t('notes', 'This category already exists')
+					return
+				}
+
+				// Create new directory for category in current notes path defined in settings
+				createCategory(categoryName)
+				this.createCategoryError = null
+				this.onToggleNewCategory()
+				this.onSelectCategory(categoryName)
+			})
+
 		},
 	},
 }
