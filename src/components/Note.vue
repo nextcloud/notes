5d13a77c702aff92e3b307e379e86add8871676c
Julius Härtl <jus@bitgrid.net>
2023-04-29 11:28:55 +0200

    fix: Avoid loading plain editor during initial load
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -1,6 +1,6 @@
 <template>
-	<NoteRich v-if="rich" :note-id="noteId" />
-	<NotePlain v-else :note-id="noteId" />
+	<NoteRich v-if="ready && rich" :note-id="noteId" />
+	<NotePlain v-else-if="ready" :note-id="noteId" />
 </template>
 <script>
 import NoteRich from './NoteRich.vue'
@@ -25,6 +25,9 @@ export default {
 	computed: {
 		rich() {
 			return window.oc_appswebroots.text && store.state.app.settings.noteMode === 'rich'
+		},
+		ready() {
+			return store.state.app.settings.noteMode !== null
 		},
 	},
 }
