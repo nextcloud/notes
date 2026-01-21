bf8ce904c9dbc40769766f4849d634cb9da58405
Enjeck C. <patrathewhiz@gmail.com>
2026-01-21 03:04:21 +0000

    fix: Add playwright server setup for CI
    Signed-off-by: Enjeck C. <patrathewhiz@gmail.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,20 +24,40 @@ export default defineConfig({
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('./')`. */
-		baseURL: process.env.BASE_URL ?? 'http://nextcloud.local',
+		baseURL: process.env.BASE_URL ?? "http://localhost:8089",
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-		trace: 'on-first-retry',
+		trace: "on-first-retry",
 
 		/* Capture screenshot on failure */
-		screenshot: 'only-on-failure',
+		screenshot: "only-on-failure",
+	},
+
+	webServer: {
+		// Starts the Nextcloud docker container
+		command: "npm run start:nextcloud",
+		// we use sigterm to notify the script to stop the container
+		// if it does not respond, we force kill it after 10 seconds
+		gracefulShutdown: {
+			signal: "SIGTERM",
+			timeout: 10000,
+		},
+		reuseExistingServer: !process.env.CI,
+		stderr: "pipe",
+		stdout: "pipe",
+		url: "http://127.0.0.1:8089",
+		timeout: 5 * 60 * 1000, // max. 5 minutes for creating the container
+		wait: {
+			// we wait for this line to appear in the output of the webserver until consider it done
+			stdout: /Nextcloud is now ready to use/,
+		},
 	},
 
 	projects: [
 		{
-			name: 'chromium',
+			name: "chromium",
 			use: {
-				...devices['Desktop Chrome'],
+				...devices["Desktop Chrome"],
 			},
 		},
 	],
---
+++ playwright/start-nextcloud-server.mjs
@@ -0,0 +1,51 @@
+/*!
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: MIT
+ */
+
+import {
+	configureNextcloud,
+	startNextcloud,
+	stopNextcloud,
+	waitOnNextcloud,
+} from '@nextcloud/e2e-test-server/docker'
+import { readFileSync } from 'fs'
+import { execSync } from 'node:child_process'
+
+async function start() {
+	const appinfo = readFileSync('appinfo/info.xml').toString()
+	const maxVersion = appinfo.match(
+		/<nextcloud min-version="\d+" max-version="(\d\d+)" \/>/,
+	)?.[1]
+
+	let branch = 'master'
+	if (maxVersion) {
+		const refs = execSync('git ls-remote --refs').toString('utf-8')
+		branch = refs.includes(`refs/heads/stable${maxVersion}`)
+			? `stable${maxVersion}`
+			: branch
+	}
+
+	return await startNextcloud(branch, true, {
+		exposePort: 8089,
+	})
+}
+
+async function stop() {
+	process.stderr.write('Stopping Nextcloud server…\n')
+	await stopNextcloud()
+	process.exit(0)
+}
+
+process.on('SIGTERM', stop)
+process.on('SIGINT', stop)
+
+// Start the Nextcloud docker container
+const ip = await start()
+await waitOnNextcloud(ip)
+await configureNextcloud(['notes'])
+
+// Idle to wait for shutdown
+while (true) {
+	await new Promise((resolve) => setTimeout(resolve, 5000))
+}
