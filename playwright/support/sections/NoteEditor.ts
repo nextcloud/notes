6bcf25d97a023d3d39408811e05258f7f62f5ba9
Enjeck C. <patrathewhiz@gmail.com>
2026-01-21 04:21:25 +0000

    fix: lint
    Signed-off-by: Enjeck C. <patrathewhiz@gmail.com>

--- playwright/support/sections/NoteEditor.ts
+++ playwright/support/sections/NoteEditor.ts
@@ -3,18 +3,20 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import type { Locator, Page } from "@playwright/test";
+import type { Locator, Page } from '@playwright/test'
 
 export class NoteEditor {
-	public readonly el: Locator;
-	public readonly content: Locator;
+
+	public readonly el: Locator
+	public readonly content: Locator
 
 	constructor(public readonly page: Page) {
-		this.el = this.page.locator(".editor").first();
-		this.content = this.el.getByRole("textbox");
+		this.el = this.page.locator('.editor').first()
+		this.content = this.el.getByRole('textbox')
 	}
 
 	public async type(keys: string): Promise<void> {
-		await this.content.pressSequentially(keys);
+		await this.content.pressSequentially(keys)
 	}
+
 }
