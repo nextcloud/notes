664daf7fe132b6452620fad4aee5a14e44ea5e24
Josh <josh.t.richards@gmail.com>
2026-04-26 20:25:42 -0400

    refactor: register note file event listeners
    Signed-off-by: Josh <josh.t.richards@gmail.com>

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -9,12 +9,18 @@ declare(strict_types=1);
 
 namespace OCA\Notes\AppInfo;
 
+use OCA\Notes\Listener\NoteFileEventsListener;
 use OCA\Notes\Reference\NoteReferenceProvider;
+use OCA\Notes\Service\MetaService;
 use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
 use OCP\AppFramework\Http\Events\BeforeTemplateRenderedEvent;
+use OCP\Files\Events\Node\BeforeNodeDeletedEvent;
+use OCP\Files\Events\Node\BeforeNodeRenamedEvent;
+use OCP\Files\Events\Node\BeforeNodeTouchedEvent;
+use OCP\Files\Events\Node\BeforeNodeWrittenEvent;
 use OCP\Share\Events\BeforeShareCreatedEvent;
 
 /** @phan-suppress-next-line PhanUnreferencedUseNormal */
@@ -31,20 +37,18 @@ class Application extends App implements IBootstrap {
 		$context->registerCapability(Capabilities::class);
 		$context->registerSearchProvider(SearchProvider::class);
 		$context->registerDashboardWidget(DashboardWidget::class);
-		$context->registerEventListener(
-			BeforeTemplateRenderedEvent::class,
-			BeforeTemplateRenderedListener::class
-		);
-		$context->registerEventListener(
-			BeforeShareCreatedEvent::class,
-			BeforeShareCreatedListener::class
-		);
+
+		$context->registerEventListener(BeforeTemplateRenderedEvent::class, BeforeTemplateRenderedListener::class);
+		$context->registerEventListener(BeforeShareCreatedEvent::class, BeforeShareCreatedListener::class);
+
+		$context->registerEventListener(BeforeNodeWrittenEvent::class, NoteFileEventsListener::class);
+		$context->registerEventListener(BeforeNodeTouchedEvent::class, NoteFileEventsListener::class);
+		$context->registerEventListener(BeforeNodeDeletedEvent::class, NoteFileEventsListener::class);
+		$context->registerEventListener(BeforeNodeRenamedEvent::class, NoteFileEventsListener::class);
+
 		$context->registerReferenceProvider(NoteReferenceProvider::class);
 	}
 
 	public function boot(IBootContext $context): void {
-		$context->injectFn(function (NotesHooks $notesHooks) {
-			$notesHooks->register();
-		});
 	}
 }
