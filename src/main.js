c11c6befa2d347641c038964a6dbb9e75e5072a7
silver <s.szmajduch@posteo.de>
2026-07-13 15:50:33 +0200

    chore(pinia): remove obsolete store injection in main.js
    Signed-off-by: silver <s.szmajduch@posteo.de>

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router.js'
-import store from './store.js'
 
 __webpack_nonce__ = btoa(OC.requestToken) // eslint-disable-line
 __webpack_public_path__ = OC.linkTo('notes', 'js/') // eslint-disable-line
@@ -15,7 +14,6 @@ Vue.mixin({ methods: { t, n } })
 
 export default new Vue({
 	el: '#content',
-	store,
 	router,
 	render: h => h(App),
 })
