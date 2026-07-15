6911435d71d19f2eea1193d4bcd5872e86690742
silver <s.szmajduch@posteo.de>
2026-07-15 13:46:46 +0200

    fix(eslint-config): add missing CR and license header
    Signed-off-by: silver <s.szmajduch@posteo.de>

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,8 @@
+/**
+ * SPDX-FileCopyrightText: 2020 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
 import { recommended } from '@nextcloud/eslint-config'
 
 export default [
